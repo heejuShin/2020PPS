554cd2d835dee8bd2232ee08ab32f70374078d07
heejuShin <21800412@handong.edu>
2020-08-06 16:06:55 +0900

    정리

--- 20200727/E4_신희주_20200727.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool search(vector<int>& nums, int target){
-        vector<int>::iterator it;
-        it=find(nums.begin(), nums.end(), target);
-        if(it==nums.end()) return false;
-        else return true;
-    }
-};
